685a1af695edcd7076d7740459fe2ed0679cb198
ameinias <gillian@blekkenhorst.ca>
2023-06-05 14:43:34 -0400

    Fixed sprite import settings and made "animation edition" which is just playing all the animations when clicking a button

--- tojam2023Unity/Assets/AnimatorSignal.cs
+++ tojam2023Unity/Assets/AnimatorSignal.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AnimatorSignal : MonoBehaviour
 {
@@ -11,11 +12,26 @@ public class AnimatorSignal : MonoBehaviour
     public bool Attack;
     public bool Growth;
 
-    Animator[] animators; 
+    Animator[] animators;
+
+    public GameObject stage1;
+    public GameObject stage2;
+    public GameObject stage3;
+    public int cooldown;
+    public int timer;
+
+    public Animator playerAnim;
+    public GameObject nextButton;
+    public GameObject resetButton; 
+
 
     void Start()
     {
         animators = FindObjectsByType<Animator>(FindObjectsSortMode.None);
+
+        nextButton.SetActive(true);
+        resetButton.SetActive(false);
+
     }
 
 
@@ -30,7 +46,7 @@ public class AnimatorSignal : MonoBehaviour
         if (Stage != prevStage)
         {
 
-            UpdateStage();
+            UpdateStage(Stage);
             prevStage = Stage;
         }
 
@@ -45,6 +61,13 @@ public class AnimatorSignal : MonoBehaviour
             AttackFunction();
             Attack = false;
         }
+
+        if (timer > 0)
+            timer--;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            ExitGame();
+            
     }
 
     void DoThing(string term)
@@ -55,6 +78,8 @@ public class AnimatorSignal : MonoBehaviour
 
             int currentValue = anim.GetInteger("Growth");
             anim.SetInteger("Growth", currentValue + 1);
+
+
         }
 
     }
@@ -76,15 +101,39 @@ public class AnimatorSignal : MonoBehaviour
     void Trigger(string value)
     {
 
-        foreach (Animator anim in animators)
-        {
-            anim.SetTrigger(value);
+        int rand1 = Random.Range(0, animators.Length);
+        int rand2 = Random.Range(0, animators.Length);
+        int rand3 = Random.Range(0, animators.Length);
+        int rand4 = Random.Range(0, animators.Length);
 
-        }
+        if (animators[rand1].tag == "Brick")
+            animators[rand1].SetTrigger(value);
+        if (animators[rand2].tag == "Brick")
+            animators[rand2].SetTrigger(value);
+        if (animators[rand3].tag == "Brick")
+            animators[rand3].SetTrigger(value);
+        if (animators[rand4].tag == "Brick")
+            animators[rand4].SetTrigger(value);
+
+
+        /*        foreach (Animator anim in animators)
+                {
+                    anim.SetTrigger(value);
+
+                }*/
 
     }
 
-    void UpdateStage()
+    public void TriggerAll()
+    {
+        foreach (Animator anim in animators)
+        {
+            anim.SetTrigger("PlaceBrick");
+
+        }
+    }
+
+    void UpdateStage(int Stage)
     {
         foreach (Animator anim in animators)
         {
@@ -92,6 +141,60 @@ public class AnimatorSignal : MonoBehaviour
 
         }
 
+    }
+
+    public void NextButton()
+    {
+
+        /*            public int Stage = 1;
+            int prevStage;
+            public bool PlaceBrick;
+            public bool Attack;
+            public bool Growth;*/
+
+        if (timer <= 0)
+        {
+            if (playerAnim.GetInteger("Damage") < 5) {
+                AttackFunction();
+                Trigger("PlaceBrick");
+            } else
+            {
+
+                if (playerAnim.GetInteger("Growth") < 5)
+                {
+                    AttackFunction();
+                    TriggerAll();
+                    UpdateStage(2);
+                    DoThing("Growth");
+                }
+                else
+                {
+                    UpdateStage(3);
+                    nextButton.SetActive(false);
+                    resetButton.SetActive(true);
+                }
+            }
+
+
+
+
+            timer = cooldown;
+        }
+
+
+
+}
+
+    public void Reset()
+    {
+           Stage = 1;
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 
 }
--- tojam2023Unity/Assets/MainMenuScript.cs
+++ tojam2023Unity/Assets/MainMenuScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
@@ -14,5 +15,22 @@ public class MainMenuScript : MonoBehaviour
     public void CreditPanel(bool value)
     {
         creditPanel.SetActive(value);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            ExitGame();
     }
 }
